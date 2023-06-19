ad28808354fc6fe6232022bd68f3a14589255164
Karl Anderson <kra@monkey.org>
2023-06-18 18:42:51 -0700

    empty bot lines

--- test/test_websocketserver.py
+++ test/test_websocketserver.py
@@ -17,7 +17,7 @@ class TestPoetryProgram(unittest.IsolatedAsyncioTestCase):
     async def test_bot_lines(self):
         prog = program.PoetryProgram()
         bot_lines = await prog.bot_lines(666, [])
-        self.assertTrue(isinstance(bot_lines.pop(), str))
+        self.assertEqual(bot_lines, [])
 
     async def test_latest_rhyme(self):
         prog = program.PoetryProgram()
