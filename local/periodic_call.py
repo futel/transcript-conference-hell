279666837057c4c85269af9d7dd79b67f31d16ac
Karl Anderson <kra@monkey.org>
2023-06-24 10:50:56 -0700

    no more futel calls for nw

--- local/periodic_call.py
+++ local/periodic_call.py
@@ -15,30 +15,32 @@ auth_token = os.environ['TWILIO_AUTH_TOKEN']
 from_number = '+15032126803'
 to_addresses_extensions = [
     'sip:demo1@futel-conference.sip.twilio.com',
-    'sip:demo2@futel-conference.sip.twilio.com',
+    #'sip:demo2@futel-conference.sip.twilio.com',
     'sip:demo3@futel-conference.sip.twilio.com',
-    'sip:demo4@futel-conference.sip.twilio.com',
-    'sip:demo5@futel-conference.sip.twilio.com',
+    #'sip:demo4@futel-conference.sip.twilio.com',
+    #'sip:demo5@futel-conference.sip.twilio.com',
     'sip:demo6@futel-conference.sip.twilio.com',
     'sip:demo7@futel-conference.sip.twilio.com',
-    'sip:demo8@futel-conference.sip.twilio.com',
-    'sip:demo9@futel-conference.sip.twilio.com']
+    #'sip:demo8@futel-conference.sip.twilio.com',
+    #'sip:demo9@futel-conference.sip.twilio.com'
+]
 
-to_addresses_futel = [
-    '+15039266271',
-    '+15034449412',
-    '+15039266341',
-    '+17345476651',
-    '+13602282259',
-    '+15039266188',
-    '+13132469283',
-    '+15034836584',
-    '+15033889637',
-    '+15039465227',
-    '+15032945966',
-    '+15039288465',
-    '+15034448615']
+# to_addresses_futel = [
+#     '+15039266271',
+#     '+15034449412',
+#     '+15039266341',
+#     '+17345476651',
+#     '+13602282259',
+#     '+15039266188',
+#     '+13132469283',
+#     '+15034836584',
+#     '+15033889637',
+#     '+15039465227',
+#     '+15032945966',
+#     '+15039288465',
+#     '+15034448615']
 
+# We seem to only get 6 secs anyway?
 min_ring = 6
 max_ring = 15
 
@@ -48,7 +50,7 @@ async def call(to_address):
     ring_secs = random.randint(min_ring, max_ring)
     print(to_address, ring_secs)
     # Call hangs up immediatly on pickup. User's ATA
-    # initiates a new call. This probably won't work.
+    # initiates a new call. This probably won't work:
     # twiml='<Response><Hangup/></Response>',
     call = client.calls.create(
         url="https://ws.app-dev.phu73l.net/index.xml",
@@ -64,21 +66,19 @@ async def call(to_address):
     call = client.calls(call.sid).fetch()
     if call.status != 'in-progress':
         call = client.calls(call.sid).update(status='completed')
-        print(to_address, 'canceled')
+        print(to_address, call.status, 'canceled')
     else:
         print(to_address, call.status)
 
 async def main():
-    tasks = []
     # Should simultaneously ring futel phones.
     while True:
         to_address = random.choice(to_addresses_extensions)
-        tasks.append(asyncio.create_task(call(to_address)))
-        to_address = random.choice(to_addresses_futel)
-        tasks.append(asyncio.create_task(call(to_address)))
-        to_address = random.choice(to_addresses_futel)
-        tasks.append(asyncio.create_task(call(to_address)))
-        wait_secs = random.randint(min_ring+2, max_ring+5)
+        asyncio.create_task(call(to_address))
+        # Some of these pick up always? PSTN? Or Futel?
+        #to_address = random.choice(to_addresses_futel)
+        #tasks.append(asyncio.create_task(call(to_address)))
+        wait_secs = random.randint(min_ring*2, max_ring*5)
         await asyncio.sleep(wait_secs)
 
 asyncio.run(main())
